4e381f4c1a9744ded1fbf1a11112eff1c2e6e468
patipan-patisampita <patipan.patisampita@gmail.com>
2021-11-22 17:04:41 +0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+/*
+*authorities: Mr.Chuchart Seatiew
+*Project: ESP32 LED BLINK Demo
+*Board ESP32 DevKit V1
+*On Board LED GPIO 2
+*/
+#include <Arduino.h>
+#define LED1 15 //GPIO 15
+#define LED2 2  //Gpio 2
+#define LED3 5  //GPIO 5
+
+void setup()
+{
+  // Set pin mode
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+}
+
+void loop()
+{
+  delay(2000); //2000 mili secound
+  digitalWrite(LED1, HIGH);
+  delay(2000); //2000 mili secound
+  digitalWrite(LED1, LOW);
+  delay(2000);
+  digitalWrite(LED2,HIGH);
+  delay(2000);
+  digitalWrite(LED2,LOW);
+  delay(2000);
+  digitalWrite(LED3,HIGH);
+  delay(2000);
+  digitalWrite(LED3,LOW);
+}
